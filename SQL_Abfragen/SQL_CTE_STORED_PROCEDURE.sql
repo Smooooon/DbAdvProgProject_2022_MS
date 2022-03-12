eabe3413ec1dda8438aa1b5750f1a1540754117d
zanma21 <mario.zanni21@icloud.com>
2022-03-12 22:24:32 +0100

    SQL_CTE_STORED_PROCEDURE.sql erneut einsatzfähig angepasst

--- SQL_Abfragen/SQL_CTE_STORED_PROCEDURE.sql
+++ SQL_Abfragen/SQL_CTE_STORED_PROCEDURE.sql
@@ -1,46 +1,37 @@
-/****** Skript für SelectTopNRows-Befehl aus SSMS ******/
-SELECT TOP (1000) [Id]
-      ,[Name]
-      ,[HigherLevelArticleGroupId]
-  FROM [MusterAG].[dbo].[ArticleGroups]
+CREATE PROCEDURE ArticleGroups @higherLAG int
+AS
 
- INSERT INTO ArticleGroups
-VALUES ('Wasser', 3 );
-
-SELECT * FROM dbo.ArticleGroups;
-
-use MusterAG;
-
-
-
-DELIMITER $
-create procedure ArticelSort (IN name VARCHAR(50), OUT hello VARCHAR(50))
-BEGIN
-	Select HigherLevelArticleGroupId (ID, name, ProductLevel) into ArticelSort;
-END$
-DELIMITER ;
-
-
-;WITH CTE_Products (
-	Id, HigherLevelArticleGroupId, ProductLevel )
+;WITH CTE_ArticleGroups (
+	Id, name, HigherLevelArticleGroupId, ProductLevel )
 AS (
 	SELECT	Id ,
+			name,
             HigherLevelArticleGroupId ,
-            0 AS ProductLevel
-    FROM Muster..Artikelgruppe
+            1 AS ProductLevel
+    FROM dbo.ArticleGroups
     WHERE HigherLevelArticleGroupId IS NULL
     UNION ALL
     SELECT	pn.Id ,
+			pn.name,
             pn.HigherLevelArticleGroupId ,
             p1.ProductLevel + 1
-    FROM Muster..Artikelgruppe AS pn
-    INNER JOIN CTE_Products AS p1
+    FROM dbo.ArticleGroups AS pn
+    INNER JOIN CTE_ArticleGroups AS p1
 		ON p1.Id = pn.HigherLevelArticleGroupId
 )
 
 SELECT	Id ,
-        HigherLevelArticleGroupId,
+		name,
+        HigherLevelArticleGroupId ,
         ProductLevel
-FROM CTE_Products
-ORDER BY HigherLevelArticleGroupId;
+FROM CTE_ArticleGroups
+ORDER BY ProductLevel;
 GO
+
+EXEC ArticleGroups @higherLAG = 1 ;
+
+
+
+
+Drop Procedure If Exists ArticleGroups ;
+
